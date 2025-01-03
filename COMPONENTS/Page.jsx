48560a46ecc663e59c4fb49425eb86ef2ca6264e
SedativeGlam <winnieonwuka2019@gmail.com>
2025-01-03 13:46:12 +0100

    section2 import component fix

--- COMPONENTS/Page.jsx
+++ COMPONENTS/Page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Section1 from "./Section1";
-import Section2 from "./section2";
+import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
 import Section5 from "./Section5";
